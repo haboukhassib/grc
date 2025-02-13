db20103d56ffda4a61197e6353cd94008c99a290
Hicham <haboukhassib@gmail.com>
2025-02-13 01:12:36 +0100

    Test in AWS

---
+++ src/main/java/com/company/grc/view/auditrecommendationfragment/AuditrecommendationFragment.java
@@ -0,0 +1,9 @@
+package com.company.grc.view.auditrecommendationfragment;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import io.jmix.flowui.fragment.Fragment;
+import io.jmix.flowui.fragment.FragmentDescriptor;
+
+@FragmentDescriptor("auditrecommendation-fragment.xml")
+public class AuditrecommendationFragment extends Fragment<VerticalLayout> {
+}
---
+++ src/main/java/com/company/grc/view/regulatorymonitoringfragment/RegulatorymonitoringFragment.java
@@ -0,0 +1,9 @@
+package com.company.grc.view.regulatorymonitoringfragment;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import io.jmix.flowui.fragment.Fragment;
+import io.jmix.flowui.fragment.FragmentDescriptor;
+
+@FragmentDescriptor("regulatorymonitoring-fragment.xml")
+public class RegulatorymonitoringFragment extends Fragment<VerticalLayout> {
+}
