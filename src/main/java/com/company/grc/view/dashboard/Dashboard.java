587342fc4658e6662be8154fa3879f68e7d84f31
Hicham <haboukhassib@gmail.com>
2025-02-25 17:26:34 +0000

    Jenkinsfile update

--- src/main/java/com/company/grc/view/dashboard/Dashboard.java
+++ src/main/java/com/company/grc/view/dashboard/Dashboard.java
@@ -2,13 +2,25 @@ package com.company.grc.view.dashboard;
 
 
 import com.company.grc.view.main.MainView;
+import com.vaadin.flow.component.PropertyDescriptor;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.StandardView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.view.*;
+import io.jmix.supersetflowui.component.SupersetDashboard;
+import org.springframework.beans.factory.annotation.Value;
 
 @Route(value = "dashboard", layout = MainView.class)
 @ViewController(id = "Dashboard")
 @ViewDescriptor(path = "dashboard.xml")
 public class Dashboard extends StandardView {
+
+    @Value("${superset.dashboard.embeddedId}")
+    private String embId;
+
+    @ViewComponent
+    private SupersetDashboard dashboard;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        dashboard.setEmbeddedId(embId);
+         }
 }
