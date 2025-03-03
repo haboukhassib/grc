39512815da42ec9f765ac57637379958bbf18df6
Hicham <haboukhassib@gmail.com>
2025-03-03 16:56:42 +0000

    Set Sub-Organization Role Polices

--- src/main/java/com/company/grc/security/SubOrganizationRole.java
+++ src/main/java/com/company/grc/security/SubOrganizationRole.java
@@ -1,9 +1,6 @@
 package com.company.grc.security;
 
-import com.company.grc.entity.AuditRecommendation;
-import com.company.grc.entity.Organization;
-import com.company.grc.entity.RegulatoryMonitoring;
-import com.company.grc.entity.Reporting;
+import com.company.grc.entity.*;
 import io.jmix.security.model.EntityAttributePolicyAction;
 import io.jmix.security.model.EntityPolicyAction;
 import io.jmix.security.role.annotation.EntityAttributePolicy;
@@ -16,8 +13,8 @@ import io.jmix.securityflowui.role.annotation.ViewPolicy;
 public interface SubOrganizationRole {
     String CODE = "sub-organization-role";
 
-    @EntityAttributePolicy(entityClass = AuditRecommendation.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
-    @EntityPolicy(entityClass = AuditRecommendation.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.CREATE, EntityPolicyAction.DELETE})
+    @EntityAttributePolicy(entityClass = AuditRecommendation.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = AuditRecommendation.class, actions = EntityPolicyAction.ALL)
     void auditRecommendation();
 
     @EntityAttributePolicy(entityClass = RegulatoryMonitoring.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
@@ -30,10 +27,26 @@ public interface SubOrganizationRole {
     void reporting();
 
     @MenuPolicy(menuIds = "Reporting.list")
-    @ViewPolicy(viewIds = {"Reporting.list", "AlertFragment", "Audit_.detail", "Audit_.list", "FiltrageFragment", "kycFragment", "MainView", "LoginView", "OrganizationFragment", "RegulatoryMonitoring.detail", "RegulatoryMonitoring.list", "Reporting.detail"})
+    @ViewPolicy(viewIds = {"Reporting.list", "AlertFragment", "Audit_.detail", "Audit_.list", "FiltrageFragment", "kycFragment", "MainView", "LoginView", "OrganizationFragment", "RegulatoryMonitoring.detail", "RegulatoryMonitoring.list", "Reporting.detail", "AuditRecommendation.detail", "AuditRecommendation.list", "AuditrecommendationFragment", "CentifFragment", "CtrlconfFragment", "FatcaFragment", "IncidentTool.detail", "IncidentTool.list", "IncidenttoolFragment", "NewProductActivity.detail", "NewProductActivity.list", "NewproductactivityFragment", "PdpFragment", "RegulatorymonitoringFragment", "SanctionPenalty.detail", "SanctionPenalty.list", "SanctionpenaltyFragment", "TrainingAwareness.detail", "TrainingAwareness.list", "TrainingawarenessFragment", "headerPropertyFilterLayout", "flowui_AddConditionView", "flowui_GroupFilterCondition.detail", "flowui_JpqlFilterCondition.detail", "flowui_PropertyFilterCondition.detail", "inputDialog", "multiValueSelectDialog", "flowui_DateIntervalDialog", "FragmentRenderer"})
     void screens();
 
     @EntityAttributePolicy(entityClass = Organization.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
     @EntityPolicy(entityClass = Organization.class, actions = EntityPolicyAction.READ)
     void organization();
+
+    @EntityAttributePolicy(entityClass = IncidentTool.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = IncidentTool.class, actions = EntityPolicyAction.ALL)
+    void incidentTool();
+
+    @EntityAttributePolicy(entityClass = NewProductActivity.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = NewProductActivity.class, actions = EntityPolicyAction.ALL)
+    void newProductActivity();
+
+    @EntityAttributePolicy(entityClass = SanctionPenalty.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = SanctionPenalty.class, actions = EntityPolicyAction.ALL)
+    void sanctionPenalty();
+
+    @EntityAttributePolicy(entityClass = TrainingAwareness.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = TrainingAwareness.class, actions = EntityPolicyAction.ALL)
+    void trainingAwareness();
 }
