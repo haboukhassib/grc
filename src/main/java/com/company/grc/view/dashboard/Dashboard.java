8464983b4326f5d6dd941ed9ea3de30e4438b245
Hicham ABOUKHASSIB <haboukhassib@gmail.com>
2025-03-08 14:37:56 +0000

    Upgrade JMIX 2.5

--- src/main/java/com/company/grc/view/dashboard/Dashboard.java
+++ src/main/java/com/company/grc/view/dashboard/Dashboard.java
@@ -23,5 +23,6 @@ public class Dashboard extends StandardView {
     @Subscribe
     public void onInit(final InitEvent event) {
         dashboard.setEmbeddedId(embId);
+        System.out.println(embId);
          }
 }
